f178633f097f267f348708bf2766e4fc3277a49c
zmq1287 <849997846@qq.com>
2020-04-04 21:05:39 +0800

    修改路径

--- application/manager_addr/config.php
+++ application/manager_addr/config.php
@@ -2,12 +2,12 @@
 //配置文件
 return [
     'view_replace_str'  => [
-        '__PUBLIC__'    =>  '/public/',
-        '__STATIC__'    =>  '/public/static/',
-        '__JS__'        =>  '/public/static/manager/js/',
-        '__CSS__'       =>  '/public/static/manager/css/',
-        '__IMG__'       =>  '/public/static/manager/images/',
-        '__LIBRARY__'     =>  '/public/library/'
+        '__PUBLIC__'    =>  '/',
+        '__STATIC__'    =>  '/static/',
+        '__JS__'        =>  '/static/manager/js/',
+        '__CSS__'       =>  '/static/manager/css/',
+        '__IMG__'       =>  '/static/manager/images/',
+        '__LIBRARY__'   =>  '/library/'
     ]
 
 ];
